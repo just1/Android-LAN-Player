29e14c05a6c3b3405716d6d1fec7badf5fa23364
尹建华 <yinjianhua@cvte.cn>
2014-10-23 15:45:16 +0800

    从分支里面合并到master

--- app/src/com/cvte/lanplayer/utils/MediaPlayerUtil.java
+++ app/src/com/cvte/lanplayer/utils/MediaPlayerUtil.java
@@ -1,5 +1,68 @@
 package com.cvte.lanplayer.utils;
 
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+
+import com.cvte.lanplayer.R;
+import com.cvte.lanplayer.constant.AppConstant;
+import com.cvte.lanplayer.service.PlayerService;
+
 public class MediaPlayerUtil {
+	private Intent startIntent;
+	//private ActivityReceive activityReceive;
+	private static Activity mActivity;
+	private static MediaPlayerUtil instance = null;
+
+	/**
+	 * 私有默认构造子
+	 */
+	private MediaPlayerUtil() {
+//		// 创建IntentFilter
+//		IntentFilter filter = new IntentFilter();
+//		// 指定BroadcastReceiver监听的Action
+//		filter.addAction(AppConstant.MusicPlayVariate.UPDATE_ACTION);
+//		// 注册BroadcastReceiver
+//		mActivity.registerReceiver(activityReceive, filter);
+//		startIntent = new Intent(mActivity, PlayerService.class);
+//		// 启动后台Service
+//		mActivity.startService(startIntent);
+
+	}
+
+	/**
+	 * 静态工厂方法
+	 */
+	public static synchronized MediaPlayerUtil getInstance(Activity activity) {
+
+		mActivity = activity;
+		if (instance == null) {
+			instance = new MediaPlayerUtil();
+		}
+
+		return instance;
+	}
+
+	public void NextMusic() {
+
+	}
+
+	public void PauseMusic() {
+
+	}
+
+	public void PlayMusic() {
+
+	}
+
+	public void GetMusicList() {
+
+	}
+	
+	
+
+	
 
 }
--- app/src/com/cvte/lanplayer/view/LocalPlayerFragment.java
+++ app/src/com/cvte/lanplayer/view/LocalPlayerFragment.java
@@ -38,12 +38,22 @@ public class LocalPlayerFragment extends Fragment implements
 	private Activity activity;
 	private ListView mListView;
 	private static ListViewAdapter lvAdapter;
-	private UIManager mUIManager;
 
 	private Intent startIntent;
 	private ActivityReceive activityReceive;
 
 	private View myView;
+
+	// 页面控件
+	private ImageButton mImgBtnListPlay;
+	private ImageButton mImgBtnListPlayNext;
+	private SeekBar mListSeekBar;
+	private TextView mTxtListPlayName;
+	private ImageButton mImgBtnArtistPhoto;
+
+	// 根据播放进度更改进度条
+	private Handler seekbarHandler = new Handler();
+	private Thread seekbarThread;
 
 	@Override
 	public void onAttach(Activity activity) {
@@ -70,13 +80,11 @@ public class LocalPlayerFragment extends Fragment implements
 	@Override
 	public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
 		// 初始化音乐文件
-		getMyList();
+		getMusicList();
 
 		lvAdapter = new ListViewAdapter(activity,
 				AppConstant.MusicPlayData.myMusicList);
 		mListView.setAdapter(lvAdapter);
-
-		mUIManager = new UIManager();
 
 		activityReceive = new ActivityReceive();
 		// 创建IntentFilter
@@ -101,7 +109,7 @@ public class LocalPlayerFragment extends Fragment implements
 					AppConstant.MusicPlayData.CURRENT_PLAY_INDEX = position;
 					// 广播播放
 					SendBroadcast(AppConstant.MusicPlayControl.MUSIC_CONTROL_PLAY);
-				
+
 				} else {
 					if (AppConstant.MusicPlayState.CURRENT_PLAY_STATE == AppConstant.MusicPlayState.PLAY_STATE_PAUSE) {
 						// 暂停状态，则继续播放
@@ -114,12 +122,136 @@ public class LocalPlayerFragment extends Fragment implements
 			}
 
 		});
+		InitView();
+		InitListener();
+
+		StartReflashProgressThread();
+
+	}
+
+	private void InitView() {
+		mImgBtnListPlay = (ImageButton) myView
+				.findViewById(R.id.music_list_play);
+		mImgBtnListPlayNext = (ImageButton) myView
+				.findViewById(R.id.music_list_playNext);
+		mListSeekBar = (SeekBar) myView.findViewById(R.id.music_list_seekBar);
+		mTxtListPlayName = (TextView) myView.findViewById(R.id.txt_musicname);
+		mImgBtnArtistPhoto = (ImageButton) myView
+				.findViewById(R.id.music_artist_photo);
+
+	}
+
+	private void InitListener() {
+		/*
+		 * 播放按钮
+		 */
+		mImgBtnListPlay.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				if (AppConstant.MusicPlayState.CURRENT_PLAY_STATE == AppConstant.MusicPlayState.PLAY_STATE_PLAYING) {
+					// 当前为播放状态，则暂停
+					SendBroadcast(AppConstant.MusicPlayControl.MUSIC_CONTROL_PAUSE);
+				} else {
+					// 当前为暂停状态，则播放
+					SendBroadcast(AppConstant.MusicPlayControl.MUSIC_CONTROL_PLAY);
+				}
+			}
+
+		});
+
+		/*
+		 * 下一首
+		 */
+		mImgBtnListPlayNext.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				SendBroadcast(AppConstant.MusicPlayControl.MUSIC_CONTROL_NEXT);
+			}
+
+		});
+
+		/*
+		 * 列表进度条事件
+		 */
+		mListSeekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+
+			@Override
+			public void onProgressChanged(SeekBar arg0, int position,
+					boolean arg2) {
+				// TODO Auto-generated method stub
+				if (arg2 == true) {
+					AppConstant.MusicPlayData.CURRENT_PLAY_POSITION = position;
+				}
+			}
+
+			@Override
+			public void onStartTrackingTouch(SeekBar arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onStopTrackingTouch(SeekBar arg0) {
+				// TODO Auto-generated method stub
+				SendBroadcast(AppConstant.MusicPlayControl.MUSIC_CONTROL_SEEKBAR);
+			}
+
+		});
+
+	}
+
+	private void StartReflashProgressThread() {
+		seekbarThread = new Thread() {
+			@Override
+			public void run() {
+				while (true) {
+					try {
+						Thread.sleep(250);
+						seekbarHandler.post(r);
+					} catch (Throwable t) {
+
+					}
+				}
+			}
+		};
+		// 启动线程
+		seekbarThread.start();
+	}
+
+	Runnable r = new Runnable() {
+		@Override
+		public void run() {
+			// 0-正在播放
+			if (AppConstant.MusicPlayState.CURRENT_PLAY_STATE == AppConstant.MusicPlayState.PLAY_STATE_PLAYING) {
+				// 获取歌曲播放到多少ms
+				AppConstant.MusicPlayData.CURRENT_PLAY_POSITION = PlayerService
+						.getMusicCurrPosition();
+
+				// 设置进度条当前位置
+				mListSeekBar
+						.setProgress(AppConstant.MusicPlayData.CURRENT_PLAY_POSITION);
+
+			}
+			// 每隔250ms更新一次
+			// seekbarHandler.postDelayed(r, 250);
+		}
+	};
+
+	/*
+	 * 设置ListView列表中播放状态图标 currIndex当前播放索引 currState当前播放状态
+	 */
+	public void setCurrPlayState(int currIndex, int currState) {
+		lvAdapter.setPlayState(currIndex, currState);
 	}
 
 	/*
 	 * 获取音乐列表
 	 */
-	private void getMyList() {
+	private void getMusicList() {
 		// ArrayList<MusicData> list = null;
 		AppConstant.MusicPlayData.myMusicList.clear();
 		// 清除所有歌曲信息
@@ -171,7 +303,6 @@ public class LocalPlayerFragment extends Fragment implements
 			AppConstant.MusicPlayData.myMusicList.add(_song);
 		}
 		cur.close();
-
 	}
 
 	/*
@@ -192,21 +323,20 @@ public class LocalPlayerFragment extends Fragment implements
 					AppConstant.MusicPlayVariate.MUSIC_PLAY_DATA, -1);
 			switch (playState) {
 			case AppConstant.MusicPlayState.PLAY_STATE_PLAYING:
-				mUIManager.mImgBtnListPlay
+				mImgBtnListPlay
 						.setImageResource(R.drawable.handle_pause_normal);
 
 				if (playIndex >= 0) {
-					mUIManager.setCurrPlayState(
+					setCurrPlayState(
 							AppConstant.MusicPlayData.CURRENT_PLAY_INDEX,
 							AppConstant.MusicPlayState.PLAY_STATE_PLAYING);
 				}
 				break;
 			case AppConstant.MusicPlayState.PLAY_STATE_PAUSE:
-				mUIManager.mImgBtnListPlay
-						.setImageResource(R.drawable.handle_play_normal);
+				mImgBtnListPlay.setImageResource(R.drawable.handle_play_normal);
 
 				if (playIndex >= 0) {
-					mUIManager.setCurrPlayState(
+					setCurrPlayState(
 							AppConstant.MusicPlayData.CURRENT_PLAY_INDEX,
 							AppConstant.MusicPlayState.PLAY_STATE_PAUSE);
 				}
@@ -219,17 +349,15 @@ public class LocalPlayerFragment extends Fragment implements
 				AppConstant.MusicPlayData.CURRENT_PLAY_POSITION = PlayerService
 						.getMusicCurrPosition();
 				// 歌曲名称
-				mUIManager.mTxtListPlayName
-						.setText(AppConstant.MusicPlayData.myMusicList.get(
-								AppConstant.MusicPlayData.CURRENT_PLAY_INDEX)
-								.getFileName());
+				mTxtListPlayName.setText(AppConstant.MusicPlayData.myMusicList
+						.get(AppConstant.MusicPlayData.CURRENT_PLAY_INDEX)
+						.getFileName());
 
 				// /设置歌曲索引
 				// mUIManager.mSliderDrawerManager.mTxtSongNum.setText(String.format("%d/%d",
 				// AppConstant.mCurrentIndex+1,AppConstant.mySongList.size()));
 				// 进度条
-				mUIManager.mListSeekBar
-						.setMax(PlayerService.getMusicDuration());
+				mListSeekBar.setMax(PlayerService.getMusicDuration());
 			}
 		}
 	}
@@ -253,148 +381,6 @@ public class LocalPlayerFragment extends Fragment implements
 		activity.sendBroadcast(sendIntent);
 	}
 
-	public class UIManager extends Activity {
-
-		private Handler seekbarHandler = new Handler();
-		private Thread seekbarThread;
-
-		private ImageButton mImgBtnListPlay;
-		private ImageButton mImgBtnListPlayNext;
-		private SeekBar mListSeekBar;
-		private TextView mTxtListPlayName;
-		private ImageButton mImgBtnArtistPhoto;
-
-		public UIManager() {
-			initUIManager();
-		}
-
-		private void initUIManager() {
-			mImgBtnListPlay = (ImageButton) myView
-					.findViewById(R.id.music_list_play);
-			mImgBtnListPlayNext = (ImageButton) myView
-					.findViewById(R.id.music_list_playNext);
-			mListSeekBar = (SeekBar) myView
-					.findViewById(R.id.music_list_seekBar);
-			mTxtListPlayName = (TextView) myView
-					.findViewById(R.id.txt_musicname);
-			mImgBtnArtistPhoto = (ImageButton) myView
-					.findViewById(R.id.music_artist_photo);
-
-			// 开启新线程
-			StartThread();
-
-			/*
-			 * 播放按钮
-			 */
-			mImgBtnListPlay.setOnClickListener(new OnClickListener() {
-
-				@Override
-				public void onClick(View arg0) {
-					// TODO Auto-generated method stub
-					if (AppConstant.MusicPlayState.CURRENT_PLAY_STATE == 
-							AppConstant.MusicPlayState.PLAY_STATE_PLAYING) {
-						// 当前为播放状态，则暂停
-						SendBroadcast(AppConstant.MusicPlayControl.MUSIC_CONTROL_PAUSE);
-					} else {
-						// 当前为暂停状态，则播放
-						SendBroadcast(AppConstant.MusicPlayControl.MUSIC_CONTROL_PLAY);
-					}
-				}
-
-			});
-
-			/*
-			 * 下一首
-			 */
-			mImgBtnListPlayNext.setOnClickListener(new OnClickListener() {
-
-				@Override
-				public void onClick(View arg0) {
-					// TODO Auto-generated method stub
-					SendBroadcast(AppConstant.MusicPlayControl.MUSIC_CONTROL_NEXT);
-				}
-
-			});
-
-			/*
-			 * 列表进度条事件
-			 */
-			mListSeekBar
-					.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
-
-						@Override
-						public void onProgressChanged(SeekBar arg0,
-								int position, boolean arg2) {
-							// TODO Auto-generated method stub
-							if (arg2 == true) {
-								AppConstant.MusicPlayData.CURRENT_PLAY_POSITION = position;
-							}
-						}
-
-						@Override
-						public void onStartTrackingTouch(SeekBar arg0) {
-							// TODO Auto-generated method stub
-
-						}
-
-						@Override
-						public void onStopTrackingTouch(SeekBar arg0) {
-							// TODO Auto-generated method stub
-							SendBroadcast(AppConstant.MusicPlayControl.MUSIC_CONTROL_SEEKBAR);
-						}
-
-					});
-
-		}
-
-		/*
-		 * 初始化线程
-		 */
-		private void StartThread() {
-			seekbarThread = new Thread() {
-				@Override
-				public void run() {
-					while (true) {
-						try {
-							Thread.sleep(250);
-							seekbarHandler.post(r);
-						} catch (Throwable t) {
-
-						}
-					}
-				}
-			};
-			// 启动线程
-			seekbarThread.start();
-		}
-
-		Runnable r = new Runnable() {
-			@Override
-			public void run() {
-				// 0-正在播放
-				if (AppConstant.MusicPlayState.CURRENT_PLAY_STATE == AppConstant.MusicPlayState.PLAY_STATE_PLAYING) {
-					// 获取歌曲播放到多少ms
-					AppConstant.MusicPlayData.CURRENT_PLAY_POSITION = PlayerService
-							.getMusicCurrPosition();
-
-					// 设置进度条当前位置
-					mListSeekBar
-							.setProgress(AppConstant.MusicPlayData.CURRENT_PLAY_POSITION);
-
-				}
-				// 每隔250ms更新一次
-				// seekbarHandler.postDelayed(r, 250);
-			}
-		};
-
-		/*
-		 * 设置ListView列表中播放状态图标 currIndex当前播放索引 currState当前播放状态
-		 */
-		public void setCurrPlayState(int currIndex, int currState) {
-			lvAdapter.setPlayState(currIndex, currState);
-		}
-	}
-
 	public void DisplayToast(String str) {
 		Toast.makeText(activity, str, Toast.LENGTH_SHORT).show();
 	}
@@ -402,7 +388,6 @@ public class LocalPlayerFragment extends Fragment implements
 	@Override
 	public void OnServiceConnectComplete() {
 		// TODO Auto-generated method stub
-
 	}
 
 }
